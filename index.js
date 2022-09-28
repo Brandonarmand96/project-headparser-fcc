a7553fba7e996487b15abb91b564db46ea2f3fba
Replit user <>
2022-09-28 16:25:12 +0100

    let this work

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  software = req.headers['user-agent'];
+  language = req.headers['accept-language'];
+  ipaddress = req.headers['x-forwarded-for']
+  res.json({ "ipaddress": ipaddress, "language": language, "software": software});
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
